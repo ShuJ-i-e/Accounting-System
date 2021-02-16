2be751b7835fe439cb07b9005d772e0a25d23be1
Shujie <shujie001202@gmail.com>
2021-02-16 20:47:28 +0800

    add product controller

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // $products = Product::latest()->get();
+
+        // return view('product')->with('products', $products);
+
+        $products = Product::latest()->paginate(5);
+    
+        return view('product.productView',compact('products'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('product.productAdd');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'prodName' => 'required',
+        ]);
+    
+        Product::create($request->all());
+     
+        return redirect()->action([ProductController::class, 'index'])
+                        ->with('success','Product inserted successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $resource = Product::find($product)->first();
+        return view('product')->with('resource', $resource);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'prodName' => 'required',
+        ]);
+    
+        $product->update($request->all());
+    
+        return redirect()->action([ProductController::class, 'index'])
+                        ->with('success','Product updated successfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $product->delete();
+        return redirect()->action([ProductController::class, 'index'])
+                        ->with('success','Product deleted successfully.');
+    }
+}
