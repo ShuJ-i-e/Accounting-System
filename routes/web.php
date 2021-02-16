24ca4d4e43449ff4cc2a1c19f855e184a2e0a781
Shujie <shujie001202@gmail.com>
2021-02-16 23:55:31 +0800

    update product and company route

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('main');
 });
+
+Route::resource('product', ProductController::class);
+Route::resource('company', CompanyController::class);
