9f9ed786b577b1905a87a48f721291a03f6554fb
Shujie <shujie001202@gmail.com>
2021-02-16 20:44:12 +0800

    create database migration

---
+++ database/migrations/2021_02_16_054744_invoice.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Invoice extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoice', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->text('companyId');
+            $table->decimal('invTotal', 8, 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoice');
+    }
+}
---
+++ database/migrations/2021_02_16_055230_order.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Order extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->text('prodId');
+            $table->text('invId');
+            $table->decimal('weight', 8, 2);
+            $table->decimal('price', 8, 2);
+            $table->decimal('total', 8, 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order');
+    }
+}
