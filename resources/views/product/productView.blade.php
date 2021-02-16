50dcd0c4879ed335d09008676ae69da270e49f4f
Shujie <shujie001202@gmail.com>
2021-02-16 20:46:59 +0800

    add product view page

---
+++ resources/views/product/productView.blade.php
@@ -0,0 +1,39 @@
+@extends('main')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Products</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-success" href="/product/create"> Create New Product</a>
+            </div>
+        </div>
+    </div>
+   
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+   <br>
+    <table class="table table-bordered">
+        <tr>
+            <th>Product ID</th>
+            <th>Name</th>
+            <th>Details</th>
+            <th width="280px">Action</th>
+        </tr>
+        @foreach ($products as $product)
+        <tr>
+            <td>{{ $product->id }}</td>
+            <td>{{ $product->prodName }}</td>
+            <td>
+            </td>
+        </tr>
+        @endforeach
+    </table>
+  
+      
+@endsection
