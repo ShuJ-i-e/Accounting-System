69cbc137694668f68d60c7b42107b5236c8808f0
Shujie <shujie001202@gmail.com>
2021-02-23 00:05:05 +0800

    add payment migration

---
+++ database/migrations/2021_02_21_043208_payment.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Payment extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payment', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('companyId');
+            $table->decimal('payment', 8, 2);
+            $table->foreign('companyId')->references('id')->on('company')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
