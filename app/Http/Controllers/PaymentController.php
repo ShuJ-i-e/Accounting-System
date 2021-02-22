dc4ef58960ea4764c1659404068480515b90f619
Shujie <shujie001202@gmail.com>
2021-02-23 00:04:51 +0800

    add controller setCompanyId and create

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+use DB;
+use App\Models\Payment;
+use App\Models\Company;
+use Illuminate\Http\Request;
+
+class PaymentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    public function setCompanyId(Request $request)
+    {
+        $resource = DB::table('company')
+        ->where('id', '=', $request->id)
+        ->value('total');
+        return response()->json(['success'=>'Data is successfully added', 'resource'=> $resource]);
+        // return view('payment.create', compact('resource'));
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+        $companies = Company::all();
+
+        if ($request->has('id')) 
+        {
+            $invoices = DB::table('invoice')
+            ->join('company', 'company.id', '=', 'invoice.companyId')
+            ->select('invoice.id', 'company.companyName', 'invoice.invTotal')
+            ->where('company.companyId', '=', $request->companyId);
+            return view('payment.createMain', compact('invoices', 'companies'));     
+        }
+        return view('payment.create', compact('companies'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Payment  $payment
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Payment $payment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Payment  $payment
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Payment $payment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Payment  $payment
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Payment $payment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Payment  $payment
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Payment $payment)
+    {
+        //
+    }
+}
